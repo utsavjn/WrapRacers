93ef29e34ab188c5dd16d1420efdd75c84c84a94
William <WilliamGMaciejewski@outlook.com>
2017-04-18 10:20:56 +1000

    201704

---
+++ Assets/Scripts/GameData/PowerUpsData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace GameData
+{
+    public class PowerUpsData : GameData<PowerUpsData>
+    {
+        public string name { get; protected set; }
+        public string type { get; protected set; }
+        public string stringfullpath { get; protected set; }
+
+        public PowerUpsData()
+        {
+
+        }
+
+        static public readonly string fileName = "xml/GameData - Items";
+    }
+}
---
+++ Assets/Scripts/Player/Stats.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface Stats {
+	
+ 	void Hit(int damage);
+
+	void Die();
+}
