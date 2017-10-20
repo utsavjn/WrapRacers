b811841aa0b02f66a54add7ce833ee75220f2c43
Utsav Jain <8211313+utsavjn@users.noreply.github.com>
2017-10-19 21:10:45 -0700

    Add files via upload

---
+++ Assets/LaserAnimator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserAnimator : MonoBehaviour {
+
+	Animator a;
+
+	void Awake()
+	{
+		a = GetComponent<Animator>();
+	}
+
+	public void Disable()
+	{
+		a.SetBool("fire", false);
+		gameObject.SetActive(false);
+	}
+}
